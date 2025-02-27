361393dcc4c83c5da61f3490d6a2be6552cc6c3c
RimandaPanjaitan <panjaitanrimanda@gmail.com>
2025-02-27 14:50:34 +0700

    Kami sudah selesai pak

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,33 @@
-// NIM - Name
-// NIM - Name
+// 12S24049 - Ika Maria Manurung
+// 12S24049 - Rimanda Santa Risa Panjaitan
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-int main(int _argv, char **_argc) {
-  // codes
+int main(){
+
+  char str1[61];
+  fgets(str1, 61, stdin);
+  str1[strcspn(str1, "\n")] = 0;
+
+  if (strlen(str1) < strlen(str1) > 60){
+    printf("invalid\n");
+    return 0;
+  }
+
+  int x;
+  char massage[21];
+
+  for(x = 0; x < strlen(str1)/3; x++){
+    char temp[4];
+    strncpy(temp, str1 + x * 3, 3);
+    temp[3] = 0;
+    massage[x] = (char) atoi (temp);
+  }
+  massage[x] = 0;
+  printf("%s\n", massage);
 
   return 0;
 }
+
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,23 @@
-// NIM - Name
-// NIM - Name
+// 12S24049 - Ika Maria Manurung
+// 12S24049 - Rimanda Santa Risa Panjaitan
 
 #include <stdio.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
 
+  char str[61];
+  fgets(str, sizeof(str), stdin);
+
+  size_t len = strlen(str);
+  if (len > 0 && str[len-1] == '\n') {
+    str [--len] = '\0';
+    len --;
+  }
+  for (int i =0; i < len; i++) {
+    printf ("%03d\n", (int) str[i]);
+  }
+  printf("\n");
+  printf("013");
   return 0;
 }
